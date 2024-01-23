d224c42c1c2e479b4e1faaa723906d0dd198b142
acruz1 <acruz@student.neumont.edu>
2024-01-23 10:26:52 -0700

    Implemented Unit Test Project

---
+++ NimUnitTest/Pages/Shared/NimTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TheGameOfNim.Pages.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheGameOfNim.Pages.Shared.Tests
+{
+    [TestClass()]
+    public class NimTest
+    {
+        [TestMethod()]
+        public void makeRowTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
---
+++ NimUnitTest/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace NimUnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
