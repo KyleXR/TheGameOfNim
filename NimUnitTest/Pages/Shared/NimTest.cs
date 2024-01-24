9dc0adaa589ae6b36cdefc6515e7209dd9170885
acruz1 <acruz@student.neumont.edu>
2024-01-24 09:48:13 -0700

    New Tests

--- NimUnitTest/Pages/Shared/NimTest.cs
+++ NimUnitTest/Pages/Shared/NimTest.cs
@@ -11,10 +11,58 @@ namespace TheGameOfNim.Pages.Shared.Tests
     [TestClass()]
     public class NimTest
     {
-        [TestMethod()]
-        public void makeRowTest()
+        [DataTestMethod]
+        [DataRow(1, 0)]
+        public void MatchesAreAbleToBeRemovedFromRow(int numToSubtract, int rowSelected)
         {
-            Assert.Fail();
+            Nin test = new Nin();
+
+            var actual = test.canTake(numToSubtract, rowSelected);
+
+            Assert.IsTrue(actual);
+        }
+
+        [DataTestMethod]
+        [DataRow(2, 0)]
+        public void MatchesAreNotAbleToBeRemovedFromRow(int numToSubtract, int rowSelected)
+        {
+            Nin test = new Nin();
+
+            var actual = test.canTake(numToSubtract, rowSelected);
+
+            Assert.IsFalse(actual);
+        }
+
+        /*[DataTestMethod]
+        [DataRow(null, null)]
+        public void NumbersAndRowsMustNotBeEmpty(int numToSubtract, int rowSelected)
+        {
+            Nin test = new Nin();
+
+
+        }*/
+
+        [DataTestMethod]
+        [DataRow(0, 1)]
+        [DataRow(1, 2)]
+        [DataRow(2, 4)]
+        [DataRow(3, 6)]
+        public void MatchesHaveBeenRemovedAndEqualExpected(int rowSelected, int numToRemove)
+        {
+            Nin test = new Nin();
+
+            int[] expected = test.rows;
+            expected[rowSelected] -= numToRemove;
+            
+            test.takeFromRows(rowSelected, numToRemove);
+            int[] actual = test.rows;
+
+            for(int i = 0; i < expected.Length; i++)
+            {
+                Console.WriteLine(expected[i]);
+                Console.WriteLine(actual[i]);
+                Assert.AreEqual(expected[i], actual[i]);
+            }
         }
     }
 }
--- TheGameOfNim/Pages/Shared/Nin.cs
+++ TheGameOfNim/Pages/Shared/Nin.cs
@@ -1,9 +1,35 @@
 ﻿using System.Globalization;
 
-namespace TheGameOfNim.Pages.Shared{
+namespace TheGameOfNim.Pages.Shared
+{
 
-    public class Nin{
+    public class Nin
+    {
 
         public int[] rows = { 1, 3, 5, 7 };
-    } 
+
+        public bool canTake(int numToSubtract, int rowSelected)
+        {
+
+            if (rows[rowSelected] == 0) return false;
+
+            if (rows[rowSelected] - numToSubtract >= 0) return true;
+
+            return false;
+
+        }
+
+        public void takeFromRows(int rowSelected, int numToSubtract)
+        {
+
+            if (canTake(numToSubtract, rowSelected) == true)
+            {
+
+                rows[rowSelected] -= numToSubtract;
+
+            }
+
+        }
+
+    }
 }
